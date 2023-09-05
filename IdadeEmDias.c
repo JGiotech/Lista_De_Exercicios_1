6927a843416b3a4e7f7b6f92fac9c36b5bad3e75
Joelma Giovanna <142359801+JGiotech@users.noreply.github.com>
2023-09-05 13:39:45 -0300

    IdadeEmDias.c

---
+++ IdadeEmDias.c
@@ -0,0 +1,19 @@
+int main(){
+
+    int idadeEmDias, ano, mes, dia;
+
+    scanf("%d", &idadeEmDias);
+
+    ano = (idadeEmDias/365);
+    mes = (idadeEmDias -  ano*365)/30;
+    dia = (idadeEmDias - ano*365 - mes*30);
+
+    printf("%d ano(s)\n", ano);
+    
+    printf("%d mes(es)\n", mes);
+
+    printf("%d dia(s)\n", dia);
+    
+
+return 0;
+}
